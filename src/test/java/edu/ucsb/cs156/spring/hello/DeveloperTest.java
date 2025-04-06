57c70ec507a5015740fb6a81c2f3928d2eb06b51
Saul Diaz <saul_diaz@ucsb.edu>
2025-04-05 22:16:22 -0700

    SD - finished adding the tests for id and team

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,10 +25,30 @@ public class DeveloperTest {
     
         assertEquals("Saul D.", Developer.getName());
     }
+    
+    @Test
     public void getGithubId_returns_correct_githubId() {
         
         assertEquals("Sauld04", Developer.getGithubId());
     }
-   
+
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("s25-11", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Jonah S."), "Team should contain Jonah S.");
+        assertTrue(t.getMembers().contains("Luis M."), "Team should contain Luis M.");
+        assertTrue(t.getMembers().contains("Shuang"), "Team should contain Shuang");
+        assertTrue(t.getMembers().contains("Tristan R."), "Team should contain Tristan R.");
+        assertTrue(t.getMembers().contains("Saul D."), "Team should contain Saul D.");
+        assertTrue(t.getMembers().contains("Wendy S."), "Team should contain Wendy S.");
+    }
+
+
 
 }
